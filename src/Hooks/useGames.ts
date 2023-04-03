176d2d9d5ed142e5afdf899dd4e958d1ad1bd0f5
TechRami <techrami@outlook.com>
2023-04-03 03:01:27 +0300

    created a custom hook for fetching games data

---
+++ src/Hooks/useGames.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+export interface GameProps {
+    id: number; // id of the game
+    name: string; // name of the game
+  }
+  
+export interface GamesResponseData {
+    count: number; // total number of games
+    results: GameProps[]; // array of games
+  }
+
+const useGames = () => {
+    const [games, setGames] = useState<GameProps[]>([]); // array of games
+    const [error, setError] = useState(""); // error message
+    
+    useEffect(() => {
+        const controller = new AbortController();
+      apiClient
+        .get<GamesResponseData>("/games", {signal: controller.signal})
+        .then((res) => setGames(res.data.results))
+        .catch((err) => {
+            if(err instanceof CanceledError) return;
+            setError(err.message)});
+        
+        return () => controller.abort();
+    }, []);
+    return { games, error}
+}
+
+export default useGames;
+
