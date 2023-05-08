6ee2c79246a575cbd0355ab4664c142a94b64f53
TechRami <techrami@outlook.com>
2023-05-08 17:00:56 +0300

    added game trailer

---
+++ src/Components/GameTrailer.tsx
@@ -0,0 +1,24 @@
+import useGameTrailer from "../Hooks/useGameTrailer";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useGameTrailer(gameId);
+
+  if (isLoading) return <p>isLoading ...</p>;
+
+  if (error) throw error;
+
+  const firstItem = data?.results[0];
+  return firstItem ? (
+    <video
+      src={firstItem.data[480]}
+      poster={firstItem.preview}
+      controls
+    ></video>
+  ) : null;
+};
+
+export default GameTrailer;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,9 +5,11 @@ import ExpandedText from "../Components/ExpandedText";
 import GameDetail from "../Components/GameDetail";
 import GameMetaCritic from "../Components/GameMetaCritic";
 import GameAttributes from "../Components/GameAttributes";
+import useGameTrailer from "../Hooks/useGameTrailer";
+import GameTrailer from "../Components/GameTrailer";
 
 const GameDetailPage = () => {
-  const { slug } = useParams();
+  const { slug, id } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <p>...Loading</p>;
@@ -19,6 +21,7 @@ const GameDetailPage = () => {
       <h1>{game.name}</h1>
       <ExpandedText children={game.description_raw} />
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
       {/* <p>
         {game.parent_platforms.map((e) => (
           <p>{e.platform.name}</p>
