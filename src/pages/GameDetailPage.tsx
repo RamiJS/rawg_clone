e6affb12dc548967fadca76ca74bd3147096ae3e
TechRami <techrami@outlook.com>
2023-05-08 17:19:48 +0300

    styling

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -9,7 +9,7 @@ import useGameTrailer from "../Hooks/useGameTrailer";
 import GameTrailer from "../Components/GameTrailer";
 
 const GameDetailPage = () => {
-  const { slug, id } = useParams();
+  const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <p>...Loading</p>;
@@ -18,10 +18,17 @@ const GameDetailPage = () => {
 
   return (
     <div className="h-screen">
-      <h1>{game.name}</h1>
-      <ExpandedText children={game.description_raw} />
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
+      <div className="py-8 grid grid-cols-1 2xl:grid-cols-2 gap-6 items-center">
+        <div className="flex flex-col gap-5 order-2 2xl:order-1">
+          <h1 className="text-2xl lg:text-4xl font-bold ">{game.name}</h1>
+          <ExpandedText children={game.description_raw} />
+          <GameAttributes game={game} />
+        </div>
+        <div className="order-1 2xl:order-2">
+          <GameTrailer gameId={game.id} />
+        </div>
+      </div>
+
       {/* <p>
         {game.parent_platforms.map((e) => (
           <p>{e.platform.name}</p>
